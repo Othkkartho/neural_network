9979b6441491c7f6489d2c4b708aeebfade9a8a6
qhtkdrma <kimwonkyu15@gmail.com>
2023-03-30 00:02:39 +0900

    중복 코드 함수 처리 및 주석 추가 등 코드 최적화

--- src/assignment/train_neuralnet_ass.py
+++ src/assignment/train_neuralnet_ass.py
@@ -57,20 +57,12 @@ def two_layer(hidden1=0):
             test_acc = network.accuracy(x_test, t_test)
             train_acc_list.append(train_acc)
             test_acc_list.append(test_acc)
-            print("train_acc: ", train_acc, ", test_acc: ", test_acc)
+            # print("train_acc: ", train_acc, ", test_acc: ", test_acc)
 
     # 그래프 그리기
-    # markers = {'train': 'o', 'test': 's'}
-    # x = np.arange(len(train_acc_list))
-    # plt.plot(x, train_acc_list, label='train acc')
-    # plt.plot(x, test_acc_list, label='test acc', linestyle='--')
-    # plt.xlabel("epochs")
-    # plt.ylabel("accuracy")
-    # plt.ylim(0, 1.0)
-    # plt.legend(loc='lower right')
-    # plt.show()
+    # grape1(train_acc_list, test_acc_list)
 
-    hidden_list = [hidden1, 0, 0]
+    hidden_list = [hidden1]
     train_avg = sum(train_acc_list[-10:]) / 10
     test_avg = sum(test_acc_list[-10:]) / 10
 
@@ -81,9 +73,7 @@ def three_layer(hidden1=0, hidden2=0):
     # 데이터 읽기
     (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
 
-    # 2~4 계층까지 데이터 생성해 저장. 4 계층 이후는 정확도 올라가는 속도가 너무 늦거나, 안올라 가기 때문에 이후는 안만듬. 2~4 까지만 하면 될 듯, 레이어 노드 수를 변경하려면 hidden_size를 변경
-    # input_size를 변경한다면 dataset 파일 mnist_ass 코드의 img_size 변수도 변경시켜야 함. (변경 추천 수치, 392, 196)
-    network = ThreeLayerNet(input_size=784, hidden_size1=100, hidden_size2=50, output_size=10)
+    network = ThreeLayerNet(input_size=784, hidden_size1=hidden1, hidden_size2=hidden2, output_size=10)
 
     # 하이퍼파라미터
     iters_num = 10000  # 반복 횟수 설정
@@ -105,7 +95,7 @@ def three_layer(hidden1=0, hidden2=0):
         t_batch = t_train[batch_mask]
 
         # 기울기 계산
-        # grad = network.numerical_gradient(x_batch, t_batch)   # 수치 미분 방식 <- 원래는 이거 써야함. 그런데 속도가 상당히 느림. 만약 하다가 열받으면 그냥 아래꺼 쓸것
+        # grad = network.numerical_gradient(x_batch, t_batch)
         grad = network.gradient(x_batch, t_batch)  # 오차역전파 방식(속도 빠름)
 
         # 계층 별로 매개변수 갱신
@@ -122,20 +112,12 @@ def three_layer(hidden1=0, hidden2=0):
             test_acc = network.accuracy(x_test, t_test)
             train_acc_list.append(train_acc)
             test_acc_list.append(test_acc)
-            print("train_acc: ", train_acc, ", test_acc: ", test_acc)
+            # print("train_acc: ", train_acc, ", test_acc: ", test_acc)
 
     # 그래프 그리기
-    # markers = {'train': 'o', 'test': 's'}
-    # x = np.arange(len(train_acc_list))
-    # plt.plot(x, train_acc_list, label='train acc')
-    # plt.plot(x, test_acc_list, label='test acc', linestyle='--')
-    # plt.xlabel("epochs")
-    # plt.ylabel("accuracy")
-    # plt.ylim(0, 1.0)
-    # plt.legend(loc='lower right')
-    # plt.show()
+    # grape1(train_acc_list, test_acc_list)
 
-    hidden_list = [hidden1, hidden2, 0]
+    hidden_list = [hidden1, hidden2]
     train_avg = sum(train_acc_list[-10:]) / 10
     test_avg = sum(test_acc_list[-10:]) / 10
 
@@ -146,74 +128,7 @@ def four_layer(hidden1=0, hidden2=0, hidden3=0):
     # 데이터 읽기
     (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
 
-    # 2~4 계층까지 데이터 생성해 저장. 4 계층 이후는 정확도 올라가는 속도가 너무 늦거나, 안올라 가기 때문에 이후는 안만듬. 2~4 까지만 하면 될 듯, 레이어 노드 수를 변경하려면 hidden_size를 변경
-    # input_size를 변경한다면 dataset 파일 mnist_ass 코드의 img_size 변수도 변경시켜야 함. (변경 추천 수치, 392, 196)
-    network = FourLayerNet(input_size=784, hidden_size1=100, hidden_size2=75, hidden_size3=50, output_size=10)
-
-    # 하이퍼파라미터
-    iters_num = 10000  # 반복 횟수 설정
-    train_size = x_train.shape[0]
-    batch_size = 100  # 미니배치 크기
-    learning_rate = 0.1
-
-    train_loss_list = []
-    train_acc_list = []
-    test_acc_list = []
-
-    # 1 에폭당 반복 수
-    iter_per_epoch = max(train_size / batch_size, 1)
-
-    for i in range(iters_num):
-        # 미니배치 획득
-        batch_mask = np.random.choice(train_size, batch_size)
-        x_batch = x_train[batch_mask]
-        t_batch = t_train[batch_mask]
-
-        # 기울기 계산
-        # grad = network.numerical_gradient(x_batch, t_batch)   # 수치 미분 방식 <- 원래는 이거 써야함. 그런데 속도가 상당히 느림. 만약 하다가 열받으면 그냥 아래꺼 쓸것
-        grad = network.gradient(x_batch, t_batch)  # 오차역전파 방식(속도 빠름)
-
-        # 계층 별로 매개변수 갱신
-        for key in ('W1', 'b1', 'W2', 'b2', 'W3', 'b3', 'W4', 'b4'):
-            network.params[key] -= learning_rate * grad[key]
-
-        # 학습 경과 기록
-        loss = network.loss(x_batch, t_batch)
-        train_loss_list.append(loss)
-
-        # 1 에폭당 정확도 계산
-        if i % iter_per_epoch == 0:
-            train_acc = network.accuracy(x_train, t_train)
-            test_acc = network.accuracy(x_test, t_test)
-            train_acc_list.append(train_acc)
-            test_acc_list.append(test_acc)
-            print("train_acc: ", train_acc, ", test_acc: ", test_acc)
-
-    # 그래프 그리기
-    # markers = {'train': 'o', 'test': 's'}
-    # x = np.arange(len(train_acc_list))
-    # plt.plot(x, train_acc_list, label='train acc')
-    # plt.plot(x, test_acc_list, label='test acc', linestyle='--')
-    # plt.xlabel("epochs")
-    # plt.ylabel("accuracy")
-    # plt.ylim(0, 1.0)
-    # plt.legend(loc='lower right')
-    # plt.show()
-
-    hidden_list = [hidden1, hidden2, hidden3]
-    train_avg = sum(train_acc_list[-10:]) / 10
-    test_avg = sum(test_acc_list[-10:]) / 10
-
-    return hidden_list, train_acc_list, test_acc_list, train_avg, test_avg
-
-
-def five_layer(self=0):
-    # 데이터 읽기
-    (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
-
-    # 2~4 계층까지 데이터 생성해 저장. 4 계층 이후는 정확도 올라가는 속도가 너무 늦거나, 안올라 가기 때문에 이후는 안만듬. 2~4 까지만 하면 될 듯, 레이어 노드 수를 변경하려면 hidden_size를 변경
-    # input_size를 변경한다면 dataset 파일 mnist_ass 코드의 img_size 변수도 변경시켜야 함. (변경 추천 수치, 392, 196)
-    network = FiveLayerNet(input_size=784, hidden_size1=100, hidden_size2=75, hidden_size3=50, hidden_size4=25,
+    network = FourLayerNet(input_size=784, hidden_size1=hidden1, hidden_size2=hidden2, hidden_size3=hidden3,
                            output_size=10)
 
     # 하이퍼파라미터
@@ -236,7 +151,62 @@ def five_layer(self=0):
         t_batch = t_train[batch_mask]
 
         # 기울기 계산
-        # grad = network.numerical_gradient(x_batch, t_batch)   # 수치 미분 방식 <- 원래는 이거 써야함. 그런데 속도가 상당히 느림. 만약 하다가 열받으면 그냥 아래꺼 쓸것
+        # grad = network.numerical_gradient(x_batch, t_batch)
+        grad = network.gradient(x_batch, t_batch)  # 오차역전파 방식(속도 빠름)
+
+        # 계층 별로 매개변수 갱신
+        for key in ('W1', 'b1', 'W2', 'b2', 'W3', 'b3', 'W4', 'b4'):
+            network.params[key] -= learning_rate * grad[key]
+
+        # 학습 경과 기록
+        loss = network.loss(x_batch, t_batch)
+        train_loss_list.append(loss)
+
+        # 1 에폭당 정확도 계산
+        if i % iter_per_epoch == 0:
+            train_acc = network.accuracy(x_train, t_train)
+            test_acc = network.accuracy(x_test, t_test)
+            train_acc_list.append(train_acc)
+            test_acc_list.append(test_acc)
+            # print("train_acc: ", train_acc, ", test_acc: ", test_acc)
+
+    # 그래프 그리기
+    # grape1(train_acc_list, test_acc_list)
+
+    hidden_list = [hidden1, hidden2, hidden3]
+    train_avg = sum(train_acc_list[-10:]) / 10
+    test_avg = sum(test_acc_list[-10:]) / 10
+
+    return hidden_list, train_acc_list, test_acc_list, train_avg, test_avg
+
+
+def five_layer(self=0):
+    # 데이터 읽기
+    (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
+
+    network = FiveLayerNet(input_size=784, hidden_size1=100, hidden_size2=75, hidden_size3=50, hidden_size4=25,
+                           output_size=10)
+
+    iters_num = 10000
+    train_size = x_train.shape[0]
+    batch_size = 100
+    learning_rate = 0.1
+
+    train_loss_list = []
+    train_acc_list = []
+    test_acc_list = []
+
+    # 1 에폭당 반복 수
+    iter_per_epoch = max(train_size / batch_size, 1)
+
+    for i in range(iters_num):
+        # 미니배치 획득
+        batch_mask = np.random.choice(train_size, batch_size)
+        x_batch = x_train[batch_mask]
+        t_batch = t_train[batch_mask]
+
+        # 기울기 계산
+        # grad = network.numerical_gradient(x_batch, t_batch)
         grad = network.gradient(x_batch, t_batch)  # 오차역전파 방식(속도 빠름)
 
         # 계층 별로 매개변수 갱신
@@ -253,18 +223,10 @@ def five_layer(self=0):
             test_acc = network.accuracy(x_test, t_test)
             train_acc_list.append(train_acc)
             test_acc_list.append(test_acc)
-            print("train_acc: ", train_acc, ", test_acc: ", test_acc)
+            # print("train_acc: ", train_acc, ", test_acc: ", test_acc)
 
     # 그래프 그리기
-    # markers = {'train': 'o', 'test': 's'}
-    # x = np.arange(len(train_acc_list))
-    # plt.plot(x, train_acc_list, label='train acc')
-    # plt.plot(x, test_acc_list, label='test acc', linestyle='--')
-    # plt.xlabel("epochs")
-    # plt.ylabel("accuracy")
-    # plt.ylim(0, 1.0)
-    # plt.legend(loc='lower right')
-    # plt.show()
+    # grape1(train_acc_list, test_acc_list)
 
     hidden_list = [100, 75, 50, 25]
     train_avg = sum(train_acc_list[-10:]) / 10
@@ -273,40 +235,14 @@ def five_layer(self=0):
     return hidden_list, train_acc_list, test_acc_list, train_avg, test_avg
 
 
+# 각 레이어 시작 함수들
 def two_train_start(start, end, skip):
     acc_avg_list = []
 
     for i in range(start, end, skip):
         acc_avg_list.append(two_layer(hidden1=i))
 
-    train_avg_list = []
-    test_avg_list = []
-    for i in range(0, len(acc_avg_list)):
-        train_avg_list.append(acc_avg_list[i][3])
-        test_avg_list.append(acc_avg_list[i][4])
-
-    markers = {'train': 'o', 'test': 's'}
-    x = np.arange(len(train_avg_list))+1
-    plt.plot(x, train_avg_list, 'bo-', label='train avg')
-    plt.plot(x, test_avg_list, 'bo--', label='test avg')
-    plt.xlabel("hidden node")
-    plt.ylabel("accuracy")
-    plt.ylim(min(np.min(train_avg_list), np.min(test_avg_list))-0.02, 1)
-    plt.legend(loc='lower right')
-    plt.show()
-
-    count = 0
-    for train_avg in train_avg_list:
-        if train_avg == max(train_avg_list):
-            print('two layer highest avg acc hidden node: ', acc_avg_list[count][0],'train_acc: ', train_avg)
-
-        count += 1
-
-    count = 0
-    for test_avg in test_avg_list:
-        if test_avg == max(test_avg_list):
-            print('two layer highest avg acc hidden node: ', acc_avg_list[count][0], 'test_acc: ', test_avg)
-        count += 1
+    grape2('two layer', acc_avg_list)
 
 
 def three_train_start(start, end, skip):
@@ -316,35 +252,68 @@ def three_train_start(start, end, skip):
         for j in range(start, end, skip):
             acc_avg_list.append(three_layer(hidden1=i, hidden2=j))
 
-    train_avg_list = []
-    test_avg_list = []
-    for i in range(0, len(acc_avg_list)):
-        train_avg_list.append(acc_avg_list[i][3])
-        test_avg_list.append(acc_avg_list[i][4])
-
-    markers = {'train': 'o', 'test': 's'}
-    x = np.arange(len(train_avg_list)) + 1
-    plt.plot(x, train_avg_list, 'bo-', label='train avg')
-    plt.plot(x, test_avg_list, 'bo--', label='test avg')
-    plt.xlabel("hidden node")
-    plt.ylabel("accuracy")
-    plt.ylim(min(np.min(train_avg_list), np.min(test_avg_list)) - 0.02, 1)
-    plt.legend(loc='lower right')
-    plt.show()
+    grape2('three layer', acc_avg_list)
 
 
 def four_train_start(start, end, skip):
-    acc_av_list = []
+    acc_avg_list = []
 
     for i in range(start, end, skip):
         for j in range(start, end, skip):
             for k in range(start, end, skip):
-                acc_av_list.append(four_layer(hidden1=i, hidden2=j, hidden3=k))
+                acc_avg_list.append(four_layer(hidden1=i, hidden2=j, hidden3=k))
 
-    print(acc_av_list)
+    grape2('four layer', acc_avg_list)
 
 
+# 정확도가 박살나 굳이 경우의 수를 구하지는 않았음.
 def five_train_start():
     acc_av_list = [five_layer()]
 
-    print(acc_av_list)
+    print('five layer 에서 테스트 기준 최대 평균 정확도를 보인 노드:', acc_av_list[0][0], '평균 트레이너 정확도:', round(acc_av_list[0][3], 5), '평균 테스트 정확도:', round(acc_av_list[0][4]), 5)
+
+
+# 주어진 노드의 그래프를 그립니다.
+def grape1(train_acc_list, test_acc_list):
+    markers = {'train': 'o', 'test': 's'}
+    x = np.arange(len(train_acc_list))
+    plt.plot(x, train_acc_list, label='train acc')
+    plt.plot(x, test_acc_list, label='test acc', linestyle='--')
+    plt.xlabel("epochs")
+    plt.ylabel("accuracy")
+    plt.ylim(0, 1.0)
+    plt.legend(loc='lower right')
+    plt.show()
+
+
+# 각 정확도의 평균치를 구하고, 최대 정확도를 보인 노드 경우의 수를 출력합니다.
+def grape2(layer, acc_list):
+    train_avg_list = []
+    test_avg_list = []
+    for i in range(0, len(acc_list)):
+        train_avg_list.append(acc_list[i][3])
+        test_avg_list.append(acc_list[i][4])
+
+    markers = {'train': 'o', 'test': 's'}
+    x = np.arange(len(train_avg_list)) + 1
+    plt.plot(x, train_avg_list, label='train avg')
+    plt.plot(x, test_avg_list, label='test avg', linestyle='--')
+    plt.xlabel("hidden node")
+    plt.ylabel("accuracy")
+    plt.xlim(0, (len(train_avg_list)+1))
+    plt.ylim(min(min(np.min(train_avg_list), np.min(test_avg_list)) - 0.02, 0.9), 1)
+    plt.legend(loc='lower right')
+    plt.show()
+
+    count = 0
+    for train_avg in train_avg_list:
+        if train_avg == max(train_avg_list):
+            print(layer, '에서 트레이너 기준 최대 평균 정확도를 보인 노드:', acc_list[count][0], ', 평균 트레이너 정확도:', round(train_avg, 5), ', 평균 테스트 정확도:', round(test_avg_list[count], 5))
+
+        count += 1
+
+    count = 0
+    for test_avg in test_avg_list:
+        if test_avg == max(test_avg_list):
+            print(layer, '에서 테스트 기준 최대 평균 정확도를 보인 노드: ', acc_list[count][0], ', 평균 트레이너 정확도: ', round(train_avg_list[count], 5), ', 평균 테스트 정확도', round(test_avg, 5))
+        count += 1
--- src/assignment/train_neuralnet_main.py
+++ src/assignment/train_neuralnet_main.py
@@ -1,7 +1,8 @@
 from train_neuralnet_ass import two_train_start, three_train_start, four_train_start, five_train_start
 
 if __name__ == "__main__":
+    # 각 레이어 별로 hidden 값의 시작, 종료, 스킵할 값을 정해 넣으면 알아서 돌아감. 전부 돌려도 됨.
     two_train_start(15, 101, 5)
-    # three_train_start(15, 101, 10)
-    # four_train_start(15, 100, 5)
-    # five_train_start()
+    three_train_start(20, 101, 10)
+    four_train_start(20, 101, 20)
+    five_train_start()
