420ece1a5598ed6192d7f01a15dfab81bd9da519
qhtkdrma <kimwonkyu15@gmail.com>
2023-04-27 22:26:43 +0900

    CNN 으로 학습시키기

--- src/assignment/train_neuralnet_main.py
+++ src/assignment/train_neuralnet_main.py
@@ -2,7 +2,7 @@ from train_neuralnet_ass import two_train_start, three_train_start, four_train_s
 
 # 입력값 바꿀려면 이거랑, dataset/mnist_ass.py의 img_size 랑 바꾸면 됨. (변경 추천 수치, 392, 196)
 # input_size 바꾸고, mnist_ass.pkl이 만들어져 있다면 그거 없애고, 다시 돌릴것
-INPUT_SIZE = 784
+INPUT_SIZE = 196
 
 if __name__ == "__main__":
     # 각 레이어 별로 hidden 값의 시작, 종료, 스킵할 값을 정해 넣으면 알아서 돌아감. 전부 돌려도 됨.
